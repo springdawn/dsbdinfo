f26533cca95ab79ec52603080475a51d7c50d7b8
Takashi Okano <honiho@springdawn.info>
2013-01-11 04:52:41 +0900

    improve jQuery selector
    selector's speed up
    fix bugs that notification should be hidden couldn't hidden

--- js/info.js
+++ js/info.js
@@ -1,12 +1,10 @@
 (function(){
-
     var host, blogTitle, postsNumber;
     var storage = chrome.storage.local;
     var offsetTop, offsetLeft;
     var allowedStorageVolume = 0.5; // percentage of the total(0 to 1)
     var expire = 1000*60*60; // milliseconds
     var popup;
-
     chrome.extension.sendMessage({greeting: "hello"}, function(response) {
         var isPosts = response.isPosts;
         var isFollowers = response.isFollowers;
@@ -16,48 +14,45 @@
         var isNoticeImg = response.isNoticeImg;
         var isNoticeRight= response.isNoticeRight;
         var isBox = response.isBox;
-
         if(isPosts === "true") {
-            popup = jQuery("<div>").addClass("dsbdinfo_popup").appendTo("body");
+            popup = jQuery(document.createElement("div")).addClass("dsbdinfo_popup").appendTo("body");
             jQuery(document).on("mouseover","div.post_info>a", function(event) {
                 ifHover(jQuery(this));
             }).on("mouseout","div.post_info>a", function(event) {
                 ifHoverOut();
             });
         }
-
         if(isFollowers === "true") {
             var followersLink = jQuery("a.followers"),
             followersCount = followersLink.children("span.count");
             followersCount.hide();
             followersLink.hover(function() {followersCount.show();}, function() {followersCount.hide();});
         }
-
         if(isNoticeAll === "true") {
                 jQuery("li.notification").hide();
-                jQuery("ol#posts")[0].addEventListener('DOMNodeInserted',function(evt) {
+                jQuery("#posts")[0].addEventListener('DOMNodeInserted',function(evt) {
                     var target = jQuery(evt.target).is("li.notification")? jQuery(evt.target): null;
                     target? target.hide(): null;
                 });
         } else if(isNoticeReblog === "true" || isNoticeLike === "true" || isNoticeImg === "true") {
             if(isNoticeReblog === "true") {
-                jQuery("li.notification:has(div.notification_type_icon[class*=reblog_icon])").hide();
-                jQuery("ol#posts")[0].addEventListener('DOMNodeInserted',function(evt) {
-                    var target = jQuery(evt.target).is("li.notification:has(div.notification_type_icon[class*=reblog_icon])")? jQuery(evt.target): null;
+                jQuery(".notification_reblog").hide();
+                jQuery("#posts")[0].addEventListener('DOMNodeInserted',function(evt) {
+                    var target = jQuery(evt.target).is(".notification_reblog")? jQuery(evt.target): null;
                     target? target.hide(): null;
                 });
             }
             if(isNoticeLike === "true") {
-                jQuery("li.notification:has(div.notification_type_icon[class*=like_icon])").hide();
-                jQuery("ol#posts")[0].addEventListener('DOMNodeInserted',function(evt) {
-                    var target = jQuery(evt.target).is("li.notification:has(div.notification_type_icon[class*=like_icon])")? jQuery(evt.target): null;
+                jQuery(".notification_like").hide();
+                jQuery("#posts")[0].addEventListener('DOMNodeInserted',function(evt) {
+                    var target = jQuery(evt.target).is(".notification_like")? jQuery(evt.target): null;
                     target? target.hide(): null;
                 });
             }
             if(isNoticeImg === "true") {
                 var timer = null;
-                var imgPop = jQuery("<div>").addClass("dsbdinfo_img_popup").appendTo("body");
-                jQuery(document).on("mouseenter", "li.notification:has(.notification_right img)", function(e) {
+                var imgPop = jQuery(document.createElement("div")).addClass("dsbdinfo_img_popup").appendTo("body");
+                jQuery(document).on("mouseenter", ".notification:has('.notification_right img')", function(e) {
                     var me = this;
                     if(!timer) {
                         timer = setTimeout(function() {
@@ -66,47 +61,35 @@
                             var imgUrl = imgObj.attr("src");
                             imgUrl = imgUrl.replace("_75sq.", "_100.");
                             var offsets = imgObj.offset();
-                            imgPop.html("<img src='"+imgUrl+"'>");
-                            imgPop.css({"top":offsets.top, "left":offsets.left});
-                            imgPop.show(100);
-                        }, 50);
+                            imgPop.html("<img src='"+imgUrl+"'>").css({"top":offsets.top, "left":offsets.left}).show(100);
+                        }, 20);
                     }
-                });
-                jQuery(document).on("mouseleave", "li.notification:has(.notification_right img)", function(e) {
+                }).on("mouseleave", ".notification:has('.notification_right img')", function(e) {
                     setTimeout(function() {
-                        imgPop.hide();
-                        imgPop.html("");
-                    }, 50);
+                        imgPop.hide().html("");
+                    }, 20);
                 });
             }
         }
-
         if(isNoticeRight === "true") {
-            jQuery("ul#recommended_tumblelogs").hide();
-            jQuery("div#tumblr_radar").hide();
-            jQuery("a.add_and_remove").hide();
+            jQuery("#recommended_tumblelogs").hide();
+            jQuery("#tumblr_radar").hide();
+            jQuery(".add_and_remove").hide();
         }
-
         if(isBox === "true" && jQuery("#posts").length > 0) {
             showBox();
         }
     });
-
     storage.getBytesInUse(function(bytes){console.log(bytes + " of " + chrome.storage.local.QUOTA_BYTES + " bytes is used.")});
-
     function ifHover(obj) {
         offsetTop = obj.offset().top;
         offsetLeft = obj.offset().left;
-
         host = jQuery(obj).attr("href").replace(/http:|\//g, "");
         safeHost = host.replace(/\./g, '\.');
-
         accessStorage();
     }
-
     function ifHoverOut() {
-        popup.html("");
-        popup.hide(100);
+        popup.html("").hide(100);
     }
 
     function accessStorage() {
@@ -127,7 +110,6 @@
             }
         });
     }
-
     function getInfo() {
         jQuery.get("http://api.tumblr.com/v2/blog/" + host + "/info", {api_key: "uvmddiGyiyHKS0ZGJSVqtEinfIVnyOVp3wUtBGJYPBGrgFKi9S"}, function(data){
             blogTitle = data.response.blog.title;
@@ -142,13 +124,10 @@
             popInfo();
         });
     }
-
     function popInfo() {
-        popup.html(blogTitle + '<br>posts: ' + postsNumber);
-        popup.css({
+        popup.html(blogTitle + '<br>posts: ' + postsNumber).css({
             "top": offsetTop,
             "left": offsetLeft
         }).show(100);
     }
-
 })();
