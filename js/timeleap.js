7c781e7cebaacb476e607b48ebc73cfe94b38ffc
Takashi Okano <honiho@springdawn.info>
2013-07-02 10:49:36 +0900

    change timeleap function
    change selectable time to up to 20

--- js/timeleap.js
+++ js/timeleap.js
@@ -1,16 +1,10 @@
+localStorage["oldestPostNum"] = null;
 function getTimeFromKiritori() {
     jQuery.get("http://kiri-tori.tumblr.com", function(data) {
         var res = jQuery(data);
         var items = [];
         var posts = res.find(".post");
-        posts.each(function(i) {
-            var timeReg = /(\d{2}\/\d{2})&nbsp;(\d{2}:\d{2})/;
-            var timeString = jQuery(posts[i]).find("p").html().match(timeReg);
-            var time = timeString[1]+" "+timeString[2];
-            var link = jQuery(posts[i]).find("a.dsbdview").attr("href");
-            var leapString = "<a href='"+link+"'>"+time+"</a>";
-            items.push(leapString);
-        });
+        items = trimString(posts);
         showTimeleapBox(items);
     });
 }
@@ -23,7 +17,39 @@ function showTimeleapBox(items) {
         var item = jQuery("<li>").html(el);
         boxItems.append(item);
     });
+    boxItems.append(jQuery("<p>").addClass("dsbdinfo_timeleap load_next").html("▼"));
     box.append(boxTitle.append(boxItems)).prependTo(document.body);
 
     jQuery("#dsbdinfo_timeleap>li").hover(function() {jQuery(this).children("ul").slideDown(100)}, function() {jQuery(this).children("ul").hide()});
+    jQuery(".dsbdinfo_timeleap.load_next").one("mouseenter", loadNext);
+}
+
+function loadNext() {
+    jQuery.get("http://kiri-tori.tumblr.com/page/2", function(data) {
+        var res = jQuery(data);
+        var posts = res.find(".post");
+        var lis = trimString(posts);
+        var items = [];
+        lis.forEach(function(el) {
+            items.push(jQuery("<li>").addClass("insert").html(el));
+        });
+        jQuery("#dsbdinfo_timeleap>li>ul").find("p").remove().end().append(items).children(".insert").slideDown(100);
+    });
+}
+
+function trimString(posts) {
+    var items = [];
+    var oldestPostNum = localStorage["oldestPostNum"];
+    posts.each(function(i) {
+        var timeReg = /(\d{2}\/\d{2})&nbsp;(\d{2}:\d{2})/;
+        var timeString = jQuery(posts[i]).find("p").html().match(timeReg);
+        var time = timeString[1]+" "+timeString[2];
+        var link = jQuery(posts[i]).find("a.dsbdview").attr("href");
+        var leapString = "<a href='"+link+"'>"+time+"</a>";
+        var curPostNum = link.split("/").pop();
+        if(curPostNum>=oldestPostNum) return true;
+        localStorage["oldestPostNum"] = curPostNum;
+        items.push(leapString);
+    });
+    return items;
 }
